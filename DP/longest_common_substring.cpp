7d412ae8e96edc946a77439b2fa847b861489a96
Shresth Kaushik <kaushikshresth22@gmail.com>
2023-09-02 00:25:12 +0530

    2_sept

---
+++ DP/longest_common_substring.cpp
@@ -0,0 +1,9 @@
+// Extended version of the Longest common subsequence question :>
+/* Problem Statement => 
+
+Given two strings ‘X’ and ‘Y’, find the length of the longest common substring.
+Examples:
+Input : X = “GeeksforGeeks”, y = “GeeksQuiz”
+Output : 5
+The longest common substring is “Geeks” and is of length 5.
+*/
